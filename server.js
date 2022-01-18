ff75247723c4f2867f33c661777f97559b162e65
Kaitlyn Milliken <kaitlynmilliken@gmail.com>
2022-01-17 22:09:24 -0500

    added first query

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'AW#se4dr5ft6',
+        database: 'election'
+    },
+    console.log('Connected to the DB!')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
